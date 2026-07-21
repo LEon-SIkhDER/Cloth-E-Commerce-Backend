e99ba9da13af6ebbfde92af907f8be79bd56d5e4
LEon-SIkhDER <leondesktop7580@gmail.com>
2026-07-21 20:36:22 +0600

    category*, product

---
+++ index.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = 8000.
+const dns = require("dns")
+require('dotenv').config()
+const cors = require("cors")
+app.use(cors())
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+app.get('/', (req, res) => {
+    console.log("env:", process.env.MONGODB_USER)
+    res.send('Hello World!', process.env.MONGODB_PASS);
+});
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.7hhwads.mongodb.net/?appName=Cluster0`;
+// const uri = 'mongodb+srv://cloth-e-commerce:jyidBKCgiEWTULYX@cluster0.7hhwads.mongodb.net/?appName=Cluster0'
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+
+        const db = client.db("Cloth_E_Commerce")
+        const categoriesCollection = db.collection("Categories")
+        const productsCollection = db.collection("Products")
+        // category-----------------------------------------------------------------------------------------------
+
+        app.get("/categories", async (req, res) => {
+
+            const result = await categoriesCollection.find().toArray()
+
+            res.send(result)
+        })
+
+        app.patch("/category", async (req, res) => {
+            const { id, isActive } = req.body
+            // console.log(id, isActive)
+            const query = { _id: new ObjectId(id) }
+            let update = {}
+
+            if (isActive.toString()) {
+                update = { $set: { isActive: isActive, updatedAt: new Date() } }
+            }
+            // console.log("hello:------------",update)
+            const result = await categoriesCollection.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.post("/category", async (req, res) => {
+            const data = req.body
+            data.isActive = true
+            data.createdAt = new Date()
+            data.updatedAt = new Date()
+            const result = await categoriesCollection.insertOne(data)
+            res.send(result)
+        })
+
+        app.delete("/category/:id", async (req, res) => {
+            const { id } = req.params
+            const query = { _id: new ObjectId(id) }
+            const result = await categoriesCollection.deleteOne(query)
+            res.send(result)
+        })
+        // category special 
+
+        app.get("/categoryNames", async (req, res) => {
+            const query = { isActive: true }
+            const projection = {
+                projection: {
+                    _id: 1,
+                    name: 1
+                }
+            }
+            const namesInObject = await categoriesCollection.find(query, projection).toArray()
+            res.send(namesInObject)
+        })
+        // products api _____________________________________________________________________________
+        app.get("/products", async (req, res) => {
+
+            const namesInObject = await productsCollection.find().toArray()
+            res.send(namesInObject)
+        })
+        
+        app.post("/product", async (req, res) => {
+            const data = req.body
+            data.createdAt = new Date()
+            data.updatedAt = new Date()
+            const result = await productsCollection.insertOne(data)
+            res.send(result)
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
