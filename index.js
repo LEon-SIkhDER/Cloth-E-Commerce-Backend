22684ac7a3b331c3a85afc128b2c24af2e67e453
LEon-SIkhDER <leondesktop7580@gmail.com>
2026-07-22 23:55:10 +0600

    product Id generate & post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const crypto = require("crypto")
 const app = express()
 app.use(express.json())
 const port = 8000.
@@ -24,8 +25,10 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
-
+const getProductId = () => {
+    return "PRD-" + crypto.randomBytes(6).toString("base64url").replaceAll("_", "").replaceAll("-", "").slice(0, 6).toUpperCase()
+}
+console.log(getProductId())
 
 async function run() {
     try {
@@ -33,6 +36,8 @@ async function run() {
         const db = client.db("Cloth_E_Commerce")
         const categoriesCollection = db.collection("Categories")
         const productsCollection = db.collection("Products")
+        await productsCollection.createIndex({ productId: 1 }, { unique: true })
+
         // category-----------------------------------------------------------------------------------------------
 
         app.get("/categories", async (req, res) => {
@@ -90,13 +95,26 @@ async function run() {
             const namesInObject = await productsCollection.find().toArray()
             res.send(namesInObject)
         })
-        
+
         app.post("/product", async (req, res) => {
             const data = req.body
             data.createdAt = new Date()
             data.updatedAt = new Date()
-            const result = await productsCollection.insertOne(data)
-            res.send(result)
+            while (true) try {
+                data.productId = getProductId()
+                const variants = data.variants.map((item) => {
+                    return { ...item, sku: data.productId + item.size + item.color.split("").slice(0, 3).join("").toUpperCase() }
+                })
+                data.variants = variants
+                const result = await productsCollection.insertOne(data)
+                return res.send(result)
+            } catch (error) {
+                console.log(error.code)
+                if (error.code === 11000) {
+                    continue
+                }
+                return res.send(error)
+            }
         })
     } finally {
 
